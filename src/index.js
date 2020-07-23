84b8ceca46c269adac6a7ab86cac1ba7fd2df3ec
Abhimanyu <abhimanyuparashar2@gmail.com>
2020-07-23 19:22:18 +0530

    'Sorting_Visualizer'

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SortVisual from './SortVisual';
+import Temp from './Temp';
+import Bar from './Bar';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+    <>
+        <SortVisual />
+    </>
+    ,document.getElementById("root")
+);
